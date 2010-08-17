1a2888e9bbcb801f529a5d1091c5c176ff01618b
jpd002 <jpd002@15a10f38-2c35-455d-a993-f4d4bf960095>
2010-08-17 14:13:37 +0000

    Added RegAllocTest.
    git-svn-id: http://svn.purei.org/codegen/trunk@36 15a10f38-2c35-455d-a993-f4d4bf960095

---
+++ tests/RegAllocTest.cpp
@@ -0,0 +1,50 @@
+#include "RegAllocTest.h"
+#include "MemStream.h"
+
+#define TEST_NUMBER1 (0xDEADDEAD)
+#define TEST_NUMBER2 (0xCAFECAFE)
+
+CRegAllocTest::CRegAllocTest()
+: m_function(NULL)
+{
+
+}
+
+CRegAllocTest::~CRegAllocTest()
+{
+	delete m_function;
+}
+
+void CRegAllocTest::Compile(Jitter::CJitter& jitter)
+{
+	Framework::CMemStream codeStream;
+	jitter.SetStream(&codeStream);
+
+	jitter.Begin();
+	{		
+		for(unsigned int i = 2; i < MAX_VARS; i++)
+		{
+			jitter.PushRel(offsetof(CONTEXT, number[i - 2]));
+			jitter.PushRel(offsetof(CONTEXT, number[i - 1]));
+			jitter.Xor();
+			jitter.PullRel(offsetof(CONTEXT, number[i - 0]));
+		}
+	}
+	jitter.End();
+
+	m_function = new CMemoryFunction(codeStream.GetBuffer(), codeStream.GetSize());
+}
+
+void CRegAllocTest::Run()
+{
+	memset(&m_context, 0, sizeof(CONTEXT));
+	m_context.number[0] = TEST_NUMBER1;
+	m_context.number[1] = TEST_NUMBER2;
+	(*m_function)(&m_context);
+	TEST_VERIFY(m_context.number[0] == TEST_NUMBER1);
+	TEST_VERIFY(m_context.number[1] == TEST_NUMBER2);
+	for(unsigned int i = 2; i < MAX_VARS; i++)
+	{
+		TEST_VERIFY(m_context.number[i] == (m_context.number[i - 1] ^ m_context.number[i - 2]));
+	}
+}
---
+++ tests/RegAllocTest.h
@@ -0,0 +1,31 @@
+#ifndef _REGALLOCTEST_H_
+#define _REGALLOCTEST_H_
+
+#include "Test.h"
+#include "MemoryFunction.h"
+
+class CRegAllocTest : public CTest
+{
+public:
+						CRegAllocTest();
+	virtual				~CRegAllocTest();
+
+	void				Compile(Jitter::CJitter&);
+	void				Run();
+
+private:
+	enum MAX_VARS
+	{
+		MAX_VARS = 16,
+	};
+
+	struct CONTEXT
+	{
+		uint32 number[MAX_VARS];
+	};
+
+	CONTEXT				m_context;
+	CMemoryFunction*	m_function;
+};
+
+#endif

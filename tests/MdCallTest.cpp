68b9f30d2d67c61958d866a4c88a1745a03d8f9c
jpd002 <jpd002@15a10f38-2c35-455d-a993-f4d4bf960095>
2012-12-27 19:57:23 +0000

    Changed MdCallTest to use integer operations instead of floating-point operations.
    git-svn-id: http://svn.purei.org/codegen/trunk@115 15a10f38-2c35-455d-a993-f4d4bf960095

--- tests/MdCallTest.cpp
+++ tests/MdCallTest.cpp
@@ -15,19 +15,24 @@ CMdCallTest::~CMdCallTest()
 
 uint32 CMdCallTest::MdInputFunction(const uint128& value, uint32 mask)
 {
-	float result = 0;
+	assert(mask == FUNCTION_MASK);
+	
+	uint32 result = 0;
 	for(unsigned int i = 0; i < 4; i++)
 	{
 		if(mask & (1 << i))
 		{
-			result += value.v[i];
+			result += value.nV[i];
 		}
 	}
-	return *reinterpret_cast<uint32*>(&result);
+	return result;
 }
 
 CMdCallTest::uint128 CMdCallTest::MdOutputFunction(uint32 mask)
 {
+	assert(mask == FUNCTION_MASK);
+	mask = FUNCTION_MASK;
+	
 	uint128 result;
 	memset(&result, 0, sizeof(result));
 
@@ -35,7 +40,7 @@ CMdCallTest::uint128 CMdCallTest::MdOutputFunction(uint32 mask)
 	{
 		if(mask & (1 << i))
 		{
-			result.v[i] = static_cast<float>(1 << i);
+			result.nV[i] = (1 << i);
 		}
 	}
 
@@ -62,7 +67,7 @@ void CMdCallTest::Compile(Jitter::CJitter& jitter)
 		{
 			jitter.MD_PushRel(offsetof(CONTEXT, value0));
 			jitter.MD_PushRel(offsetof(CONTEXT, value1));
-			jitter.MD_AddS();
+			jitter.MD_AddW();
 
 			jitter.PushCst(FUNCTION_MASK);
 
@@ -87,26 +92,26 @@ void CMdCallTest::Run()
 	CONTEXT ALIGN16 context;
 	memset(&context, 0, sizeof(CONTEXT));
 	
-	context.value0.v[0] = 8.0f;
-	context.value0.v[1] = 4.0f;
-	context.value0.v[2] = 2.0f;
-	context.value0.v[3] = 1.0f;
+	context.value0.nV[0] = 8;
+	context.value0.nV[1] = 4;
+	context.value0.nV[2] = 2;
+	context.value0.nV[3] = 1;
 
-	context.value1.v[0] = 16.0f;
-	context.value1.v[1] = 8.0f;
-	context.value1.v[2] = 4.0f;
-	context.value1.v[3] = 2.0f;
+	context.value1.nV[0] = 16;
+	context.value1.nV[1] = 8;
+	context.value1.nV[2] = 4;
+	context.value1.nV[3] = 2;
 
 	(*m_function)(&context);
 
 	//Result 0
 	{
-		float result = 0;
+		uint32 result = 0;
 		for(unsigned int i = 0; i < 4; i++)
 		{
 			if(FUNCTION_MASK & (1 << i))
 			{
-				result += context.value0.v[i];
+				result += context.value0.nV[i];
 			}
 		}
 
@@ -115,12 +120,12 @@ void CMdCallTest::Run()
 
 	//Result 1
 	{
-		float result = 0;
+		uint32 result = 0;
 		for(unsigned int i = 0; i < 4; i++)
 		{
 			if(FUNCTION_MASK & (1 << i))
 			{
-				result += context.value0.v[i] + context.value1.v[i];
+				result += context.value0.nV[i] + context.value1.nV[i];
 			}
 		}
 
@@ -133,11 +138,11 @@ void CMdCallTest::Run()
 		{
 			if(FUNCTION_MASK & (1 << i))
 			{
-				TEST_VERIFY(context.result2.v[i] == static_cast<float>(1 << i));
+				TEST_VERIFY(context.result2.nV[i] == (1 << i));
 			}
 			else
 			{
-				TEST_VERIFY(context.result2.v[i] == 0);
+				TEST_VERIFY(context.result2.nV[i] == 0);
 			}
 		}
 	}
--- tests/MdCallTest.h
+++ tests/MdCallTest.h
@@ -17,7 +17,25 @@ public:
 private:
 	struct uint128
 	{
-		float v[4];
+		union
+		{
+			struct
+			{
+				uint32 nV[4];
+			};
+			struct
+			{
+				uint32 nV0;
+				uint32 nV1;
+				uint32 nV2;
+				uint32 nV3;
+			};
+			struct
+			{
+				uint64 nD0;
+				uint64 nD1;
+			};
+		};
 	};
 
 	struct CONTEXT
@@ -27,8 +45,8 @@ private:
 		uint128			value0;
 		uint128			value1;
 
-		float			result0;
-		float			result1;
+		uint32			result0;
+		uint32			result1;
 
 		ALIGN16
 

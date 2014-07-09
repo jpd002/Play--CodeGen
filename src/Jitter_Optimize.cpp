406ac7e04e6e30dea48b5d160e41d5fb02a28299
Ian Brown <bigianb@gmail.com>
2014-07-09 22:14:05 +0100

    implement OP_DIVS for 2 constants

--- src/Jitter_Optimize.cpp
+++ src/Jitter_Optimize.cpp
@@ -761,6 +761,19 @@ bool CJitter::FoldConstantOperation(STATEMENT& statement)
 		//	src2cst->m_valueLow = GetPowerOf2(src2cst->m_valueLow);
 		//}
 	}
+	else if (statement.op == OP_DIVS)
+	{
+		if (src1cst && src2cst)
+		{
+			int32 quotient = static_cast<int32>(src1cst->m_valueLow) / static_cast<int32>(src2cst->m_valueLow);
+			uint32 remainder = src1cst->m_valueLow % src2cst->m_valueLow;
+			uint64 result = static_cast<int64>(quotient) | (static_cast<uint64>(remainder) << 32);
+			statement.op = OP_MOV;
+			statement.src1 = MakeSymbolRef(MakeConstant64(result));
+			statement.src2.reset();
+			changed = true;
+		}
+	}
 	else if(statement.op == OP_CMP)
 	{
 		if(src1cst && src2cst)

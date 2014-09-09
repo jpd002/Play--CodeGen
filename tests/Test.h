0ecb919f27ce220b133d0c5b420d8eedb30348b3
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2014-09-09 02:45:03 -0400

    Force allocations for tests to be aligned on 16 bytes.

--- tests/Test.h
+++ tests/Test.h
@@ -11,4 +11,21 @@ public:
 
 	virtual void	Run()						= 0;
 	virtual void	Compile(Jitter::CJitter&)	= 0;
+
+#ifdef _MSC_VER
+
+	//Overload operator new and delete to make sure we get proper 16-byte aligned structures
+	//C++11 has support for aligned struct/classes, but VC++2013 doesn't support them
+
+	void* operator new(size_t allocSize)
+	{
+		return _aligned_malloc(allocSize, 16);
+	}
+
+	void operator delete(void* ptr)
+	{
+		_aligned_free(ptr);
+	}
+
+#endif
 };

cf74290bec1aef83076f49c65f44d54f0462c7f3
jpd002 <jpd002@15a10f38-2c35-455d-a993-f4d4bf960095>
2015-01-20 06:08:03 +0000

    Added additional test case to Call64Test (for ARM parameter passing).
    git-svn-id: http://svn.purei.org/codegen/trunk@302 15a10f38-2c35-455d-a993-f4d4bf960095

--- tests/Call64Test.cpp
+++ tests/Call64Test.cpp
@@ -3,6 +3,7 @@
 
 #define CONSTANT_1		(0x0000084108302989ull)
 #define CONSTANT_2		(0x0017227878892871ull)
+#define CONSTANT_3		(0x8687912A)
 
 CCall64Test::CCall64Test()
 {
@@ -22,6 +23,11 @@ uint64 CCall64Test::Add64(uint64 v1, uint64 v2)
 uint64 CCall64Test::Sub64(uint64 v1, uint64 v2)
 {
 	return v1 - v2;
+}
+
+uint64 CCall64Test::AddMul64(uint32 v1, uint64 v2, uint64 v3)
+{
+	return v1 + (v2 * v3);
 }
 
 void CCall64Test::Compile(Jitter::CJitter& jitter)
@@ -46,6 +52,15 @@ void CCall64Test::Compile(Jitter::CJitter& jitter)
 			jitter.Call(reinterpret_cast<void*>(&CCall64Test::Sub64), 2, Jitter::CJitter::RETURN_VALUE_64);
 			jitter.PullRel64(offsetof(CONTEXT, result1));
 		}
+
+		//Result 2
+		{
+			jitter.PushCst(CONSTANT_3);
+			jitter.PushCst64(CONSTANT_1);
+			jitter.PushCst64(CONSTANT_2);
+			jitter.Call(reinterpret_cast<void*>(&CCall64Test::AddMul64), 3, Jitter::CJitter::RETURN_VALUE_64);
+			jitter.PullRel64(offsetof(CONTEXT, result2));
+		}
 	}
 	jitter.End();
 
@@ -64,4 +79,5 @@ void CCall64Test::Run()
 
 	TEST_VERIFY(context.result0 == (CONSTANT_1 + CONSTANT_2));
 	TEST_VERIFY(context.result1 == (CONSTANT_1 - CONSTANT_2));
+	TEST_VERIFY(context.result2 == ((CONSTANT_1 * CONSTANT_2) + CONSTANT_3));
 }

398f341668286bd3d82256aa22a5547dc32e0f50
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2015-05-19 03:44:03 -0400

    Use aligned alloc for test allocation.

--- tests/Test.h
+++ tests/Test.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "AlignedAlloc.h"
 #include "Jitter.h"
 
 #define TEST_VERIFY(a) if(!(a)) { int* p = 0; (*p) = 0; }
@@ -12,20 +13,13 @@ public:
 	virtual void	Run()						= 0;
 	virtual void	Compile(Jitter::CJitter&)	= 0;
 
-#ifdef _MSC_VER
-
-	//Overload operator new and delete to make sure we get proper 16-byte aligned structures
-	//C++11 has support for aligned struct/classes, but VC++2013 doesn't support them
-
 	void* operator new(size_t allocSize)
 	{
-		return _aligned_malloc(allocSize, 16);
+		return framework_aligned_alloc(allocSize, 0x10);
 	}
 
 	void operator delete(void* ptr)
 	{
-		_aligned_free(ptr);
+		framework_aligned_free(ptr);
 	}
-
-#endif
 };

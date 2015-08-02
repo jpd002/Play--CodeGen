a77922f8bd66e2b7e0c7be73c22108ca084b7c95
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2015-08-01 23:02:08 -0400

    Improved FpuTest.

--- tests/FpuTest.cpp
+++ tests/FpuTest.cpp
@@ -21,7 +21,7 @@ void CFpuTest::Compile(Jitter::CJitter& jitter)
 		jitter.FP_PushSingle(offsetof(CONTEXT, number1));
 		jitter.FP_PushSingle(offsetof(CONTEXT, number2));
 		jitter.FP_Add();
-		jitter.FP_PullSingle(offsetof(CONTEXT, number1));
+		jitter.FP_PullSingle(offsetof(CONTEXT, resAdd));
 
 		jitter.FP_PushSingle(offsetof(CONTEXT, number2));
 		jitter.FP_PushSingle(offsetof(CONTEXT, number4));
@@ -29,61 +29,61 @@ void CFpuTest::Compile(Jitter::CJitter& jitter)
 		jitter.FP_PullSingle(offsetof(CONTEXT, resSub));
 
 		jitter.FP_PushSingle(offsetof(CONTEXT, number2));
-		jitter.FP_PushSingle(offsetof(CONTEXT, number1));
-		jitter.FP_Div();
-		jitter.FP_PullSingle(offsetof(CONTEXT, number1));
-
-		jitter.FP_PushSingle(offsetof(CONTEXT, number1));
-		jitter.FP_Rcpl();
-		jitter.FP_PullSingle(offsetof(CONTEXT, number1));
-
-		jitter.FP_PushSingle(offsetof(CONTEXT, number1));
-		jitter.FP_Neg();
-		jitter.FP_PullSingle(offsetof(CONTEXT, number2));
-
 		jitter.FP_PushSingle(offsetof(CONTEXT, number2));
+		jitter.FP_Mul();
+		jitter.FP_PullSingle(offsetof(CONTEXT, resMul));
+
+		jitter.FP_PushSingle(offsetof(CONTEXT, number1));
+		jitter.FP_PushSingle(offsetof(CONTEXT, number2));
+		jitter.FP_Div();
+		jitter.FP_PullSingle(offsetof(CONTEXT, resDiv));
+
+		jitter.FP_PushSingle(offsetof(CONTEXT, number3));
+		jitter.FP_Neg();
+		jitter.FP_PullSingle(offsetof(CONTEXT, resNeg));
+
+		jitter.FP_PushSingle(offsetof(CONTEXT, number3));
 		jitter.FP_Abs();
 		jitter.FP_PullSingle(offsetof(CONTEXT, resAbs));
 
-		jitter.FP_PushSingle(offsetof(CONTEXT, number2));
-		jitter.FP_PushSingle(offsetof(CONTEXT, number2));
-		jitter.FP_Mul();
-		jitter.FP_PullSingle(offsetof(CONTEXT, number3));
-
-		jitter.FP_PushSingle(offsetof(CONTEXT, number3));
-		jitter.FP_Sqrt();
-		jitter.FP_PullSingle(offsetof(CONTEXT, number3));
-
 		jitter.FP_PushSingle(offsetof(CONTEXT, number4));
-		jitter.FP_Rsqrt();
-		jitter.FP_PullSingle(offsetof(CONTEXT, resRsqrt));
+		jitter.FP_Sqrt();
+		jitter.FP_PullSingle(offsetof(CONTEXT, resSqrt));
 
 		jitter.FP_PushCst(4);
 		jitter.FP_Sqrt();
 		jitter.FP_PullSingle(offsetof(CONTEXT, resSqrtCst));
 
+		jitter.FP_PushSingle(offsetof(CONTEXT, number3));
+		jitter.FP_Rcpl();
+		jitter.FP_PullSingle(offsetof(CONTEXT, resRcpl));
+
+		jitter.FP_PushSingle(offsetof(CONTEXT, number4));
+		jitter.FP_Rsqrt();
+		jitter.FP_PullSingle(offsetof(CONTEXT, resRsqrt));
+
 		jitter.FP_PushSingle(offsetof(CONTEXT, number1));
-		jitter.FP_PushSingle(offsetof(CONTEXT, number2));
+		jitter.FP_PushSingle(offsetof(CONTEXT, number3));
 		jitter.FP_Max();
 		jitter.FP_PullSingle(offsetof(CONTEXT, resMax));
 
 		jitter.FP_PushSingle(offsetof(CONTEXT, number1));
-		jitter.FP_PushSingle(offsetof(CONTEXT, number2));
+		jitter.FP_PushSingle(offsetof(CONTEXT, number3));
 		jitter.FP_Min();
 		jitter.FP_PullSingle(offsetof(CONTEXT, resMin));
 
 		jitter.FP_PushSingle(offsetof(CONTEXT, number1));
-		jitter.FP_PushSingle(offsetof(CONTEXT, number2));
+		jitter.FP_PushSingle(offsetof(CONTEXT, number3));
 		jitter.FP_Cmp(Jitter::CONDITION_BL);
 		jitter.PullRel(offsetof(CONTEXT, ltTest));
 
-		jitter.FP_PushSingle(offsetof(CONTEXT, number2));
+		jitter.FP_PushSingle(offsetof(CONTEXT, number3));
 		jitter.FP_PushSingle(offsetof(CONTEXT, number1));
 		jitter.FP_Cmp(Jitter::CONDITION_BE);
 		jitter.PullRel(offsetof(CONTEXT, leTest));
 
 		jitter.FP_PushSingle(offsetof(CONTEXT, number1));
-		jitter.FP_PushSingle(offsetof(CONTEXT, resAbs));
+		jitter.FP_PushSingle(offsetof(CONTEXT, number1));
 		jitter.FP_Cmp(Jitter::CONDITION_EQ);
 		jitter.PullRel(offsetof(CONTEXT, eqTest));
 	}
@@ -97,18 +97,21 @@ void CFpuTest::Run()
 	memset(&m_context, 0, sizeof(CONTEXT));
 	m_context.number1 = 1.0;
 	m_context.number2 = 2.0;
+	m_context.number3 = -4.0f;
 	m_context.number4 = 16.0f;
 	m_function(&m_context);
-	TEST_VERIFY(m_context.number1 ==  1.5f);
-	TEST_VERIFY(m_context.number2 == -1.5f);
-	TEST_VERIFY(m_context.number1 == m_context.number3);
+	TEST_VERIFY(m_context.resAdd == 3.0f);
 	TEST_VERIFY(m_context.resSub == -14.0f);
-	TEST_VERIFY(m_context.resAbs == 1.5f);
+	TEST_VERIFY(m_context.resMul == 4.0f);
+	TEST_VERIFY(m_context.resDiv == 0.5f);
+	TEST_VERIFY(m_context.resAbs == 4.0f);
+	TEST_VERIFY(m_context.resNeg =  4.0f);
+	TEST_VERIFY(m_context.resSqrt == 4.0f)
 	TEST_VERIFY(m_context.resSqrtCst == 2.0f);
-	//Result is not exact
-	TEST_VERIFY(fabs(0.25f - m_context.resRsqrt) < 0.001f);
-	TEST_VERIFY(m_context.resMax == 1.5f);
-	TEST_VERIFY(m_context.resMin == -1.5f);
+	TEST_VERIFY(fabs(-0.25f - m_context.resRcpl) < 0.0001f);		//Result not exact
+	TEST_VERIFY(fabs(0.25f - m_context.resRsqrt) < 0.0001f);		//Result not exact
+	TEST_VERIFY(m_context.resMax == 1.0f);
+	TEST_VERIFY(m_context.resMin == -4.0f);
 	TEST_VERIFY(m_context.ltTest == 0);
 	TEST_VERIFY(m_context.leTest != 0);
 	TEST_VERIFY(m_context.eqTest != 0);

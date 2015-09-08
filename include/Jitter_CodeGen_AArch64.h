e1442cc937318b0456a18b4cb903ec7b64900474
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2015-09-08 23:16:15 +0100

    AArch64 codegen WIP.

--- include/Jitter_CodeGen_AArch64.h
+++ include/Jitter_CodeGen_AArch64.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "Jitter_CodeGen.h"
-//#include "AArch64Assembler.h"
+#include "AArch64Assembler.h"
 
 namespace Jitter
 {
@@ -19,6 +19,7 @@ namespace Jitter
 		bool            CanHold128BitsReturnValueInRegisters() const override;
 
 	private:
+		typedef std::map<uint32, CAArch64Assembler::LABEL> LabelMapType;
 		typedef void (CCodeGen_AArch64::*ConstCodeEmitterType)(const STATEMENT&);
 
 		struct CONSTMATCHER
@@ -30,11 +31,35 @@ namespace Jitter
 			ConstCodeEmitterType    emitter;
 		};
 
-		void    MarkLabel(const STATEMENT&);
+		void    LoadRelativeInRegister(CAArch64Assembler::REGISTER32, CSymbol*);
+		void    StoreRegisterInRelative(CSymbol*, CAArch64Assembler::REGISTER32);
+		void    LoadTemporaryInRegister(CAArch64Assembler::REGISTER32, CSymbol*);
+		void    StoreRegisterInTemporary(CSymbol*, CAArch64Assembler::REGISTER32);
+		
+		void    LoadMemoryInRegister(CAArch64Assembler::REGISTER32, CSymbol*);
+		void    StoreRegisterInMemory(CSymbol*, CAArch64Assembler::REGISTER32);
+		
+		CAArch64Assembler::REGISTER32    PrepareSymbolRegisterDef(CSymbol*, CAArch64Assembler::REGISTER32);
+		CAArch64Assembler::REGISTER32    PrepareSymbolRegisterUse(CSymbol*, CAArch64Assembler::REGISTER32);
+		void                             CommitSymbolRegister(CSymbol*, CAArch64Assembler::REGISTER32);
+		
+		void    Emit_Prolog(uint32);
+		void    Emit_Epilog(uint32);
+		
+		CAArch64Assembler::LABEL GetLabel(uint32);
+		void                     MarkLabel(const STATEMENT&);
+		
+		void    Emit_Mov_VarVar(const STATEMENT&);
+		
+		//SHIFT
+		void    Emit_Shift_Generic(const STATEMENT&);
 
 		static CONSTMATCHER    g_constMatchers[];
+		static CAArch64Assembler::REGISTER64    g_baseRegister;
 
 		Framework::CStream*    m_stream = nullptr;
-//		CAArch32Assembler      m_assembler;
+		CAArch64Assembler      m_assembler;
+		LabelMapType           m_labels;
+		uint32                 m_stackLevel = 0;
 	};
 };
--- src/Jitter_CodeGen_AArch64.cpp
+++ src/Jitter_CodeGen_AArch64.cpp
@@ -2,9 +2,29 @@
 
 using namespace Jitter;
 
-CCodeGen_AArch64::CONSTMATCHER CCodeGen_AArch64::g_constMatchers[] = 
+CAArch64Assembler::REGISTER64    CCodeGen_AArch64::g_baseRegister = CAArch64Assembler::x19;
+
+void CCodeGen_AArch64::Emit_Shift_Generic(const STATEMENT& statement)
 {
-	{ OP_LABEL,    MATCH_NIL,    MATCH_NIL,    MATCH_NIL,    &CCodeGen_AArch64::MarkLabel    },
+	auto dst = statement.dst->GetSymbol().get();
+	auto src1 = statement.src1->GetSymbol().get();
+	auto src2 = statement.src2->GetSymbol().get();
+
+	assert(src2->m_type == SYM_CONSTANT);
+	
+	auto dstReg = PrepareSymbolRegisterDef(dst, CAArch64Assembler::w0);
+	auto src1Reg = PrepareSymbolRegisterUse(src1, CAArch64Assembler::w1);
+	m_assembler.Asr(dstReg, src1Reg, src2->m_valueLow);
+	CommitSymbolRegister(dst, dstReg);
+}
+
+CCodeGen_AArch64::CONSTMATCHER CCodeGen_AArch64::g_constMatchers[] =
+{
+	{ OP_MOV,      MATCH_VARIABLE,    MATCH_VARIABLE,    MATCH_NIL,    &CCodeGen_AArch64::Emit_Mov_VarVar       },
+	
+	{ OP_SRA,      MATCH_ANY,         MATCH_ANY,         MATCH_ANY,    &CCodeGen_AArch64::Emit_Shift_Generic    },
+	
+	{ OP_LABEL,    MATCH_NIL,         MATCH_NIL,         MATCH_NIL,    &CCodeGen_AArch64::MarkLabel             },
 };
 
 CCodeGen_AArch64::CCodeGen_AArch64()
@@ -44,7 +64,7 @@ bool CCodeGen_AArch64::CanHold128BitsReturnValueInRegisters() const
 void CCodeGen_AArch64::SetStream(Framework::CStream* stream)
 {
 	m_stream = stream;
-//	m_assembler.SetStream(stream);
+	m_assembler.SetStream(stream);
 }
 
 void CCodeGen_AArch64::RegisterExternalSymbols(CObjectFile* objectFile) const
@@ -56,6 +76,8 @@ void CCodeGen_AArch64::GenerateCode(const StatementList& statements, unsigned in
 {
 	//Align stack size (must be aligned on 16 bytes boundary)
 	stackSize = (stackSize + 0xF) & ~0xF;
+
+	Emit_Prolog(stackSize);
 
 	for(const auto& statement : statements)
 	{
@@ -79,10 +101,167 @@ void CCodeGen_AArch64::GenerateCode(const StatementList& statements, unsigned in
 			throw std::runtime_error("No suitable emitter found for statement.");
 		}
 	}
+	
+	Emit_Epilog(stackSize);
+
+	m_assembler.ResolveLabelReferences();
+	m_assembler.ClearLabels();
+	m_labels.clear();
+}
+
+void CCodeGen_AArch64::LoadRelativeInRegister(CAArch64Assembler::REGISTER32 registerId, CSymbol* src)
+{
+	assert(src->m_type == SYM_RELATIVE);
+	assert((src->m_valueLow & 0x03) == 0x00);
+	m_assembler.Ldr(registerId, g_baseRegister, src->m_valueLow);
+}
+
+void CCodeGen_AArch64::StoreRegisterInRelative(CSymbol* dst, CAArch64Assembler::REGISTER32 registerId)
+{
+	assert(dst->m_type == SYM_RELATIVE);
+	assert((dst->m_valueLow & 0x03) == 0x00);
+	m_assembler.Str(registerId, g_baseRegister, dst->m_valueLow);
+}
+
+void CCodeGen_AArch64::LoadTemporaryInRegister(CAArch64Assembler::REGISTER32 registerId, CSymbol* src)
+{
+	assert(src->m_type == SYM_TEMPORARY);
+	m_assembler.Ldr(registerId, CAArch64Assembler::xSP, src->m_stackLocation + m_stackLevel);
+}
+
+void CCodeGen_AArch64::StoreRegisterInTemporary(CSymbol* dst, CAArch64Assembler::REGISTER32 registerId)
+{
+	assert(dst->m_type == SYM_TEMPORARY);
+	m_assembler.Str(registerId, CAArch64Assembler::xSP, dst->m_stackLocation + m_stackLevel);
+}
+
+void CCodeGen_AArch64::LoadMemoryInRegister(CAArch64Assembler::REGISTER32 registerId, CSymbol* src)
+{
+	switch(src->m_type)
+	{
+	case SYM_RELATIVE:
+		LoadRelativeInRegister(registerId, src);
+		break;
+	case SYM_TEMPORARY:
+		LoadTemporaryInRegister(registerId, src);
+		break;
+	default:
+		assert(0);
+		break;
+	}
+}
+
+void CCodeGen_AArch64::StoreRegisterInMemory(CSymbol* dst, CAArch64Assembler::REGISTER32 registerId)
+{
+	switch(dst->m_type)
+	{
+	case SYM_RELATIVE:
+		StoreRegisterInRelative(dst, registerId);
+		break;
+	case SYM_TEMPORARY:
+		StoreRegisterInTemporary(dst, registerId);
+		break;
+	default:
+		assert(0);
+		break;
+	}
+}
+
+CAArch64Assembler::REGISTER32 CCodeGen_AArch64::PrepareSymbolRegisterDef(CSymbol* symbol, CAArch64Assembler::REGISTER32 preferedRegister)
+{
+	switch(symbol->m_type)
+	{
+//	case SYM_REGISTER:
+//		return g_registers[symbol->m_valueLow];
+//		break;
+	case SYM_TEMPORARY:
+	case SYM_RELATIVE:
+		return preferedRegister;
+		break;
+	default:
+		throw std::runtime_error("Invalid symbol type.");
+		break;
+	}
+}
+
+CAArch64Assembler::REGISTER32 CCodeGen_AArch64::PrepareSymbolRegisterUse(CSymbol* symbol, CAArch64Assembler::REGISTER32 preferedRegister)
+{
+	switch(symbol->m_type)
+	{
+//	case SYM_REGISTER:
+//		return g_registers[symbol->m_valueLow];
+//		break;
+	case SYM_TEMPORARY:
+	case SYM_RELATIVE:
+		LoadMemoryInRegister(preferedRegister, symbol);
+		return preferedRegister;
+		break;
+//	case SYM_CONSTANT:
+//		LoadConstantInRegister(preferedRegister, symbol->m_valueLow);
+//		return preferedRegister;
+//		break;
+	default:
+		throw std::runtime_error("Invalid symbol type.");
+		break;
+	}
+}
+
+void CCodeGen_AArch64::CommitSymbolRegister(CSymbol* symbol, CAArch64Assembler::REGISTER32 usedRegister)
+{
+	switch(symbol->m_type)
+	{
+//	case SYM_REGISTER:
+//		assert(usedRegister == g_registers[symbol->m_valueLow]);
+//		break;
+	case SYM_TEMPORARY:
+	case SYM_RELATIVE:
+		StoreRegisterInMemory(symbol, usedRegister);
+		break;
+	default:
+		throw std::runtime_error("Invalid symbol type.");
+		break;
+	}
+}
+
+void CCodeGen_AArch64::Emit_Prolog(uint32 stackSize)
+{
+	m_assembler.Mov(g_baseRegister, CAArch64Assembler::x0);
+}
+
+void CCodeGen_AArch64::Emit_Epilog(uint32 stackSize)
+{
+	m_assembler.Ret();
+}
+
+CAArch64Assembler::LABEL CCodeGen_AArch64::GetLabel(uint32 blockId)
+{
+	CAArch64Assembler::LABEL result;
+	auto labelIterator(m_labels.find(blockId));
+	if(labelIterator == m_labels.end())
+	{
+		result = m_assembler.CreateLabel();
+		m_labels[blockId] = result;
+	}
+	else
+	{
+		result = labelIterator->second;
+	}
+	return result;
 }
 
 void CCodeGen_AArch64::MarkLabel(const STATEMENT& statement)
 {
-//	CAArch32Assembler::LABEL label = GetLabel(statement.jmpBlock);
-//	m_assembler.MarkLabel(label);
+	auto label = GetLabel(statement.jmpBlock);
+	m_assembler.MarkLabel(label);
+}
+
+void CCodeGen_AArch64::Emit_Mov_VarVar(const STATEMENT& statement)
+{
+	auto dst = statement.dst->GetSymbol().get();
+	auto src1 = statement.src1->GetSymbol().get();
+
+	auto dstReg = PrepareSymbolRegisterDef(dst, CAArch64Assembler::w0);
+	auto src1Reg = PrepareSymbolRegisterUse(src1, CAArch64Assembler::w0);
+	m_assembler.Mov(dstReg, src1Reg);
+	CommitSymbolRegister(dst, dstReg);
 }

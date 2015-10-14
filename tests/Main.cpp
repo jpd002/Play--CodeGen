87cdb741103c7507c5eb4f3c480ed842092393f8
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2015-10-13 20:22:41 -0400

    Added a few more test cases for Cmp64Test.

--- tests/Main.cpp
+++ tests/Main.cpp
@@ -97,6 +97,8 @@ static const TestFactoryFunction s_factories[] =
 	[] () { return new CCmp64Test(true,		0xFFFFFFFFF6543210ULL, 0xFFFFFFFFF567CDEFULL); },
 	[] () { return new CCmp64Test(false,	0x100000000, 0x100000000); },
 	[] () { return new CCmp64Test(true,		0x100000000, 0x100000000); },
+	[] () { return new CCmp64Test(true,		0, 0x80ULL); },
+	[] () { return new CCmp64Test(true,		0, 0xFFFFFFFFFFFFFF80ULL); },
 	[] () { return new CLogic64Test(); },
 	[] () { return new CShift64Test(0); },
 	[] () { return new CShift64Test(12); },

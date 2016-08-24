c09901ee5e54fc73dca1c1e42ab0dfc2f0ca611a
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2016-08-24 11:41:39 +0900

    Cleanup.

--- tests/MdCallTest.h
+++ tests/MdCallTest.h
@@ -10,8 +10,8 @@ public:
 						CMdCallTest();
 	virtual				~CMdCallTest();
 
-	void				Compile(Jitter::CJitter&);
-	void				Run();
+	void				Compile(Jitter::CJitter&) override;
+	void				Run() override;
 
 private:
 	struct uint128

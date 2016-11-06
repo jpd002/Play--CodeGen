a399dc005c3c0a09bd486b1f51591af5f185cea2
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2016-11-06 18:00:58 -0500

    Handle cases where total offset cannot be encoded in opcode.

--- src/Jitter_CodeGen_AArch32_Md.cpp
+++ src/Jitter_CodeGen_AArch32_Md.cpp
@@ -22,39 +22,57 @@ void CCodeGen_AArch32::LoadRelative128AddressInRegister(CAArch32Assembler::REGIS
 {
 	assert(symbol->m_type == SYM_RELATIVE128);
 
+	uint32 totalOffset = symbol->m_valueLow + offset;
+
 	uint8 immediate = 0;
 	uint8 shiftAmount = 0;
-	if(!TryGetAluImmediateParams(symbol->m_valueLow + offset, immediate, shiftAmount))
+	if(TryGetAluImmediateParams(totalOffset, immediate, shiftAmount))
 	{
-		throw std::runtime_error("Failed to build immediate for symbol.");
+		m_assembler.Add(dstReg, g_baseRegister, CAArch32Assembler::MakeImmediateAluOperand(immediate, shiftAmount));
 	}
-	m_assembler.Add(dstReg, g_baseRegister, CAArch32Assembler::MakeImmediateAluOperand(immediate, shiftAmount));
+	else
+	{
+		LoadConstantInRegister(dstReg, totalOffset);
+		m_assembler.Add(dstReg, g_baseRegister, dstReg);
+	}
 }
 
 void CCodeGen_AArch32::LoadTemporary128AddressInRegister(CAArch32Assembler::REGISTER dstReg, CSymbol* symbol, uint32 offset)
 {
 	assert(symbol->m_type == SYM_TEMPORARY128);
 
+	uint32 totalOffset = symbol->m_stackLocation + m_stackLevel + offset;
+
 	uint8 immediate = 0;
 	uint8 shiftAmount = 0;
-	if(!TryGetAluImmediateParams(symbol->m_stackLocation + m_stackLevel + offset, immediate, shiftAmount))
+	if(TryGetAluImmediateParams(totalOffset, immediate, shiftAmount))
 	{
-		throw std::runtime_error("Failed to build immediate for symbol.");
+		m_assembler.Add(dstReg, CAArch32Assembler::rSP, CAArch32Assembler::MakeImmediateAluOperand(immediate, shiftAmount));
 	}
-	m_assembler.Add(dstReg, CAArch32Assembler::rSP, CAArch32Assembler::MakeImmediateAluOperand(immediate, shiftAmount));
+	else
+	{
+		LoadConstantInRegister(dstReg, totalOffset);
+		m_assembler.Add(dstReg, CAArch32Assembler::rSP, dstReg);
+	}
 }
 
 void CCodeGen_AArch32::LoadTemporary256ElementAddressInRegister(CAArch32Assembler::REGISTER dstReg, CSymbol* symbol, uint32 offset)
 {
 	assert(symbol->m_type == SYM_TEMPORARY256);
 
+	uint32 totalOffset = symbol->m_stackLocation + m_stackLevel + offset;
+
 	uint8 immediate = 0;
 	uint8 shiftAmount = 0;
-	if(!TryGetAluImmediateParams(symbol->m_stackLocation + m_stackLevel + offset, immediate, shiftAmount))
+	if(TryGetAluImmediateParams(totalOffset, immediate, shiftAmount))
 	{
-		throw std::runtime_error("Failed to build immediate for symbol.");
+		m_assembler.Add(dstReg, CAArch32Assembler::rSP, CAArch32Assembler::MakeImmediateAluOperand(immediate, shiftAmount));
 	}
-	m_assembler.Add(dstReg, CAArch32Assembler::rSP, CAArch32Assembler::MakeImmediateAluOperand(immediate, shiftAmount));
+	else
+	{
+		LoadConstantInRegister(dstReg, totalOffset);
+		m_assembler.Add(dstReg, CAArch32Assembler::rSP, dstReg);
+	}
 }
 
 template <typename MDOP>

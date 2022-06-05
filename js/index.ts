4a1d54c731d9cdc9c2a0c84038bd9ff21074e1c5
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2022-06-04 21:05:27 -0400

    Add comment for future reference.

--- js/index.ts
+++ js/index.ts
@@ -1,3 +1,6 @@
+//Note: MdSubTest and ExternJumpTest still don't work on WebAssembly, 
+//they need to be disabled for the whole test suite to run successfully
+
 var factory = require("../../build_cmake/build_wasm/CodeGenTestSuite.js");
 factory().then((instance : any) => { 
     console.log("Test complete."); 

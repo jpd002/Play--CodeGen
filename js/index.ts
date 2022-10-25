f28639ab7db09af95393b82556f372ee49f92136
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2022-10-25 11:20:07 -0400

    Add some notes.

--- js/index.ts
+++ js/index.ts
@@ -1,10 +1,17 @@
 //Note: MdSubTest and ExternJumpTest still don't work on WebAssembly, 
 //they need to be disabled for the whole test suite to run successfully
 
+//If running this in VS code, make sure you are debugging and that you are
+//breaking on caught exceptions to get as much info as possible.
+
 import path from "path";
 
 var codeGenTestSuiteModulePath = path.join(process.cwd() + "/CodeGenTestSuite.js");
 var factory = require(codeGenTestSuiteModulePath);
 factory().then((instance : any) => {
-    console.log("Test complete."); 
+    if(process.exitCode != 0) {
+        console.log("Tests failed.");
+    } else {
+        console.log("Tests succeeded."); 
+    }
 });

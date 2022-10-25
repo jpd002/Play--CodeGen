47ab91199843ef7868b35be29310e23467cebf32
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2022-10-25 09:36:17 -0400

    Look for CodeGenTestSuite.js in working dir.
    Allows running it from different environments.

--- js/index.ts
+++ js/index.ts
@@ -1,7 +1,10 @@
 //Note: MdSubTest and ExternJumpTest still don't work on WebAssembly, 
 //they need to be disabled for the whole test suite to run successfully
 
-var factory = require("../../build_cmake/build_wasm/CodeGenTestSuite.js");
-factory().then((instance : any) => { 
+import path from "path";
+
+var codeGenTestSuiteModulePath = path.join(process.cwd() + "/CodeGenTestSuite.js");
+var factory = require(codeGenTestSuiteModulePath);
+factory().then((instance : any) => {
     console.log("Test complete."); 
 });

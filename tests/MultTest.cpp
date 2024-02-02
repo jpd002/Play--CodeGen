fe7949e72f359b329fb2d1af7a2bb47e2cbe8a1a
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2024-02-02 14:10:27 -0500

    Improve Mult test.

--- tests/MultTest.cpp
+++ tests/MultTest.cpp
@@ -1,6 +1,28 @@
 #include "MultTest.h"
 #include "MemStream.h"
 
+static constexpr uint32 CONSTANT_VALUE_1 = 0x80004040;
+static constexpr uint32 CONSTANT_VALUE_2 = 0x40408000;
+
+static constexpr int64 CONSTANT_SIGNED_RESULT = 
+	static_cast<int64>(static_cast<int32>(CONSTANT_VALUE_1)) *
+	static_cast<int64>(static_cast<int32>(CONSTANT_VALUE_2));
+
+static constexpr uint64 CONSTANT_UNSIGNED_RESULT = 
+	static_cast<uint64>(CONSTANT_VALUE_1) *
+	static_cast<uint64>(CONSTANT_VALUE_2);
+
+static constexpr uint32 RELATIVE_VALUE_1 = 0xFFFF8000;
+static constexpr uint32 RELATIVE_VALUE_2 = 0x8000FFFF;
+
+static constexpr int64 RELATIVE_SIGNED_RESULT =
+    static_cast<int64>(static_cast<int32>(RELATIVE_VALUE_1)) *
+    static_cast<int64>(static_cast<int32>(RELATIVE_VALUE_2));
+
+static constexpr uint64 RELATIVE_UNSIGNED_RESULT =
+    static_cast<uint64>(RELATIVE_VALUE_1) *
+    static_cast<uint64>(RELATIVE_VALUE_2);
+
 CMultTest::CMultTest(bool isSigned)
 : m_isSigned(isSigned)
 {
@@ -11,26 +33,26 @@ void CMultTest::Run()
 {
 	memset(&m_context, 0, sizeof(m_context));
 
-	m_context.relArg0 = 0xFFFF8000;
-	m_context.relArg1 = 0x8000FFFF;
+	m_context.relArg0 = RELATIVE_VALUE_1;
+	m_context.relArg1 = RELATIVE_VALUE_2;
 
 	m_function(&m_context);
 
 	if(!m_isSigned)
 	{
-		TEST_VERIFY(m_context.cstResultLo == 0x30200000);
-		TEST_VERIFY(m_context.cstResultHi == 0x20205020);
+		TEST_VERIFY(m_context.cstResultLo == static_cast<uint32>(CONSTANT_UNSIGNED_RESULT));
+		TEST_VERIFY(m_context.cstResultHi == static_cast<uint32>(CONSTANT_UNSIGNED_RESULT >> 32));
 
-		TEST_VERIFY(m_context.relResultLo == 0x80008000);
-		TEST_VERIFY(m_context.relResultHi == 0x8000BFFE);
+		TEST_VERIFY(m_context.relResultLo == static_cast<uint32>(RELATIVE_UNSIGNED_RESULT));
+		TEST_VERIFY(m_context.relResultHi == static_cast<uint32>(RELATIVE_UNSIGNED_RESULT >> 32));
 	}
 	else
 	{
-		TEST_VERIFY(m_context.cstResultLo == 0x30200000);
-		TEST_VERIFY(m_context.cstResultHi == 0xDFDFD020);
+		TEST_VERIFY(m_context.cstResultLo == static_cast<uint32>(CONSTANT_SIGNED_RESULT));
+		TEST_VERIFY(m_context.cstResultHi == static_cast<uint32>(CONSTANT_SIGNED_RESULT >> 32));
 		
-		TEST_VERIFY(m_context.relResultLo == 0x80008000);
-		TEST_VERIFY(m_context.relResultHi == 0x00003FFF);
+		TEST_VERIFY(m_context.relResultLo == static_cast<uint32>(RELATIVE_SIGNED_RESULT));
+		TEST_VERIFY(m_context.relResultHi == static_cast<uint32>(RELATIVE_SIGNED_RESULT >> 32));
 	}
 }
 
@@ -42,8 +64,8 @@ void CMultTest::Compile(Jitter::CJitter& jitter)
 	jitter.Begin();
 	{
 		//Cst x Cst
-		jitter.PushCst(0x80004040);
-		jitter.PushCst(0x40408000);
+		jitter.PushCst(CONSTANT_VALUE_1);
+		jitter.PushCst(CONSTANT_VALUE_2);
 
 		if(m_isSigned)
 		{
